60e2d5d866370a985cbfff324a13f0b4ca50c90c
hicoderindia <hicoder.dolly@gmail.com>
2022-01-27 21:24:52 +0530

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,16 +4,60 @@ import "./Header.scss";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Logo from "../Images/Logo.png";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
-import SearchBar from "./SearchBar";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 import { MultiProductPage, SingleProductPage, WomenCloth, Home } from "..";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "90%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "20ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 const Header = () => {
   const [value, setValue] = useState(0);
@@ -38,28 +82,68 @@ const Header = () => {
                 aria-label="menu"
                 sx={{ mr: 2 }}
               ></IconButton>
-              <Link to="/"> <img src={Logo} alt="" width="40" /></Link>
+              <Link to="/">
+                {" "}
+                <img src={Logo} alt="" width="40" />
+              </Link>
               {/* Navbar starts */}
               <Box sx={{ width: "100%", pl: 5 }}>
                 <Link to="/man" className="header--navbar">
-                  <Tab value="one" label="Man" className="header--navbar--nav" />
+                  <Tab
+                    value="one"
+                    label="Man"
+                    className="header--navbar--nav"
+                  />
                 </Link>
 
                 <Link to="/women" className="header--navbar">
-                  <Tab value="two" label="Women" className="header--navbar--nav" />
+                  <Tab
+                    value="two"
+                    label="Women"
+                    className="header--navbar--nav"
+                  />
                 </Link>
                 <Link to="/kids" className="header--navbar">
-                  <Tab value="three" label="Kids" className="header--navbar--nav" />
+                  <Tab
+                    value="three"
+                    label="Kids"
+                    className="header--navbar--nav"
+                  />
                 </Link>
                 <Link to="/home&living" className="header--navbar">
-                  <Tab value="four" label="Home & Livings" className="header--navbar--nav" />
+                  <Tab
+                    value="four"
+                    label="Home & Livings"
+                    className="header--navbar--nav"
+                  />
                 </Link>
                 <Link to="/beauty" className="header--navbar">
-                  <Tab value="five" label="Beauty" className="header--navbar--nav" />
+                  <Tab
+                    value="five"
+                    label="Beauty"
+                    className="header--navbar--nav"
+                  />
                 </Link>
               </Box>
               {/* Navbar ends */}
-              <SearchBar />
+              {/* <SearchBar /> */}
+              {/* SearchBar Starts */}
+              <Box sx={{ flexGrow: 1 }} className="searchInput">
+      <Toolbar>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            className="Search"
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+      </Toolbar>
+    </Box>
+
+              {/* SearchBar Ends */}
 
               {/* Notification Starts */}
               <Tabs
@@ -71,6 +155,7 @@ const Header = () => {
                 <Tab icon={<FavoriteBorderIcon />} label="Wishlist" />
                 <Tab icon={<ShoppingBagOutlinedIcon />} label="Bag" />
               </Tabs>
+              {/* Notification Ends */}
             </Toolbar>
           </AppBar>
         </Box>
@@ -78,8 +163,7 @@ const Header = () => {
           <Route path="/" element={<Home />} />
           <Route path="/women" element={<WomenCloth />} />
         </Routes>
-        {/* <MultiProductPage />
-    <SingleProductPage /> */}
+        {/* <SingleProductPage /> */} 
       </BrowserRouter>
     </>
   );

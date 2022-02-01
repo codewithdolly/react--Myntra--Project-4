18d9c1e1f57d3028469a0e3337459cd81d073449
hicoderindia <hicoder.dolly@gmail.com>
2022-02-01 23:57:07 +0530

    up

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,9 @@ export {default as Home} from "./Home";
 export {default as Footer} from "./Footer";
 export {default as SingleProduct} from "./SingleProduct";
 export {default as ProductsPage} from "./ProductsPage";
+export {default as MansProductsPage} from "./MansProductsPage"
+export {default as KidsProductsPage} from "./KidsProductsPage"
+export {default as KidsProductsPage} from "./KidsProductsPage"
+export {default as KidsProductsPage} from "./KidsProductsPage"
+export {default as KidsProductsPage} from "./KidsProductsPage"
+export {default as KidsProductsPage} from "./KidsProductsPage"
